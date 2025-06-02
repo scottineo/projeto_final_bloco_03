2840caebb502473e1780841f4957de5411f1f587
Henrique Machado <henriquesousa@ymail.com>
2025-06-02 11:16:28 -0300

    remoção rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
-import Home from "./pages/home/Home";
-import Landing from "./pages/landing/Landing";
-import Contato from "./pages/contato/Contato";
-import Sobre from "./pages/sobre/Sobre";
 
 function App() {
   return (
@@ -12,13 +8,7 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <div>
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/contato" element={<Contato />} />
-            {/* <Route path="/categorias" element={} /> */}
-            <Route path="/sobre" element={<Sobre />} />
-          </Routes>
+          <Routes></Routes>
         </div>
         <Footer />
       </BrowserRouter>
